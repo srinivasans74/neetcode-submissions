ccb89ff52f54b2001f8e3fcdbf81685c02ba51b2
srinivasans74 <47769589+srinivasans74@users.noreply.github.com>
2026-07-25 22:50:11 -0700

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) 
+    {
+        string cha;
+        std::stringstream ch;
+        for (string str:strs)
+        {
+
+            //ch+=str+"#";
+
+            //ch<<str.size()<<str<<"#";
+            ch << str.size() << "#" << str;
+        }
+    cha=ch.str();
+    //std::cout<<cha<<endl;
+    return cha;
+    }
+
+    vector<string> decode(string s) {
+        vector<string>result;
+        int i = 0;
+        while (i < static_cast<int>(s.size())) 
+        {
+            int delimiter = i;
+            while (s[delimiter] != '#') 
+            {
+                delimiter++;
+            }
+            int length = stoi(s.substr(i, delimiter - i));
+            int stringStart = delimiter + 1;
+            result.push_back(s.substr(stringStart, length));
+            i = stringStart + length;
+
+        }
+
+        return result;
+
+    }
+};
